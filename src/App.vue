d501615dab2e57a76c24813d276d1290ccedd4a7
Natalya Blanco <blanco@ubilabs.net>
2020-09-26 19:00:48 +0200

    feat(app): add form to order food

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <v-app>
+      <v-navigation-drawer app v-model="drawer"> </v-navigation-drawer>
+
+      <v-app-bar app>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-toolbar-title>El sabor del cucharon</v-toolbar-title>
+      </v-app-bar>
+
+      <!-- Sizes your content based upon application components -->
+      <v-main>
+        <!-- Provides the application the proper gutter -->
+        <v-container fluid>
+          <!-- If using vue-router -->
+          <router-view></router-view>
+        </v-container>
+      </v-main>
+
+      <v-footer app>
+        <!-- -->
+      </v-footer>
+    </v-app>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import Vue from "vue";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default Vue.extend({
+  name: "App",
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <v-card max-width="800">
+      <div class="d-flex" v-for="(filling, index) in fillings" :key="index">
+        <v-card-text>{{ filling.name }}</v-card-text>
+        <v-card-text>{{ filling.counter }} pack(s)</v-card-text>
+        <v-card-actions>
+          <v-btn icon @click="filling.counter--"
+            ><v-icon>mdi-minus</v-icon></v-btn
+          >
+          <v-btn icon @click="filling.counter++"
+            ><v-icon>mdi-plus</v-icon></v-btn
+          >
+        </v-card-actions>
+      </div>
+      <v-card-text>
+        <v-text-field label="Name"></v-text-field>
+        <v-text-field label="Street name and number"></v-text-field>
+        <v-text-field label="Post code"></v-text-field>
+        <v-text-field label="Email"></v-text-field>
+        <v-text-field label="Phone number"></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn class="mx-auto mb-6" color="success">Submit</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'Home',
-  components: {
-    HelloWorld,
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      counter: 0,
+      fillings: [
+        {
+          name: "Carne Mechada",
+          counter: 0,
+        },
+        {
+          name: "Carne Molida",
+          counter: 0,
+        },
+        {
+          name: "Pollo",
+          counter: 0,
+        },
+        {
+          name: "Queso",
+          counter: 0,
+        },
+      ],
+    };
   },
 });
 </script>
