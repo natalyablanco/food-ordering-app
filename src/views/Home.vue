2316ae1998e4bb504cc5e0a4b57ea3fd17d3813b
Duncan Taylor <duncantaylor@mailbox.org>
2020-09-26 20:17:36 +0200

    added counter functionality and total price

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="home">
-    <v-card max-width="800">
+    <v-card class="mx-auto" max-width="600">
       <div class="d-flex" v-for="(filling, index) in fillings" :key="index">
         <v-card-text>{{ filling.name }}</v-card-text>
         <v-card-text>{{ filling.counter }} pack(s)</v-card-text>
         <v-card-actions>
-          <v-btn icon @click="filling.counter--"
+          <v-btn icon @click="decrease(filling)"
             ><v-icon>mdi-minus</v-icon></v-btn
           >
           <v-btn icon @click="filling.counter++"
@@ -19,6 +19,7 @@
         <v-text-field label="Post code"></v-text-field>
         <v-text-field label="Email"></v-text-field>
         <v-text-field label="Phone number"></v-text-field>
+        <p class="text-h5 mt-2">Total cost: ${{ price }}</p>
       </v-card-text>
       <v-card-actions>
         <v-btn class="mx-auto mb-6" color="success">Submit</v-btn>
@@ -33,6 +34,28 @@ import Vue from "vue";
 export default Vue.extend({
   name: "Home",
   components: {},
+  computed: {
+    price() {
+      let totalPacks: number = 0;
+      let totalCost: number = 0;
+      this.fillings.forEach((filling) => {
+        totalPacks += filling.counter;
+      });
+      totalCost = totalPacks * 7;
+      if (totalCost % 3 == 0) {
+        totalCost = totalCost - (totalPacks / 3);
+      }
+      return totalCost;
+    },
+  },
+  methods: {
+    decrease(filling: { counter: number }) {
+      if (filling.counter === 0) {
+      } else {
+        filling.counter--;
+      }
+    },
+  },
   data() {
     return {
       counter: 0,
