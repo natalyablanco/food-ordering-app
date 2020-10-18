c48a29d363e52beae6553128a16dc56f6274c20f
Duncan Taylor <duncantaylor@mailbox.org>
2020-10-18 21:20:56 +0200

    added first part of validation

--- src/components/CustomerForm.vue
+++ src/components/CustomerForm.vue
@@ -1,27 +1,60 @@
 <<template>
+<v-form v-model="valid">
   <v-card-text>
-    <v-text-field v-model="customer.name" label="Name"></v-text-field>
+    <v-text-field required :rules="nameRules" v-model="customer.name" label="Name"></v-text-field>
     <v-text-field
+      required
+      :rules="streetRules"
       v-model="customer.address.street_number"
       label="Street name and number"
     ></v-text-field>
     <v-text-field
+      required
+      :rules="postcodeRules"
       v-model="customer.address.postcode"
-      label="Post code"
+      label="Postal code"
     ></v-text-field>
-    <v-text-field v-model="customer.email" label="Email"></v-text-field>
-    <v-text-field v-model="customer.number" label="Phone number"></v-text-field>
+    <v-text-field required :rules="emailRules" v-model="customer.email" label="Email"></v-text-field>
+    <v-text-field required :rules="phoneRules" v-model="customer.number" label="Phone number"></v-text-field>
   </v-card-text>
+</v-form>
+  
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 export default Vue.extend({
   name: "CustomerForm",
-  props: {
-    customer: {
-      default: {},
-    },
+  data() {
+    return {
+      valid: false,
+      nameRules: [
+        (v: string) => !!v || 'Name is required',
+      ],
+      streetRules: [
+        (v: string) => !!v || 'Street is required',
+      ],
+      postcodeRules: [
+        (v: string) => !!v || 'Postal code is required',
+      ],
+      emailRules: [
+        (v: string) => !!v || 'Email is required',
+        (v: string) => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      phoneRules: [
+        (v: string) => !!v || 'Phone number is required',
+        (v: string) => /^[0-9]+$/.test(v) || 'Phone number must be valid',
+      ],
+      customer: {
+        name: "",
+        email: "",
+        number: "",
+        address: {
+          street_number: "",
+          postcode: "",
+        },
+      },
+    }
   },
 });
 </script>
